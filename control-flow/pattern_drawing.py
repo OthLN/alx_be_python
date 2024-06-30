7d36f321ae7b2cd12e9da814635cb5a2d1d562dc
OthLN <ln.othmane@gmail.com>
2024-06-30 23:54:13 +0100

    Add files via upload

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,14 @@
 def main():
     try:
         size = int(input("Enter the size of the pattern: "))
-        for _ in range(size):
-            for _ in range(size):
+        row = 0
+        while row < size:
+            col = 0
+            while col < size:
                 print("*", end="")
-            print()  # Move to the next row
+                col += 1
+            print()
+            row += 1
     except ValueError:
         print("Please enter a valid positive integer.")
 
