dfd9c0c8e0043a3350ee1bb74f053425e93e9b1b
OthLN <ln.othmane@gmail.com>
2024-07-06 01:32:43 +0100

    Add files via upload

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,24 +5,22 @@ def main():
 
     time_bound = input("Is it time-bound? (yes/no): ").strip().lower()
 
-    print("\nReminder:")
-
     match priority:
         case 'high':
             if time_bound == 'yes':
-                print(f"'{task}' is a high priority task that requires immediate attention today!")
+                print(f"Reminder:\n'{task}' is a high priority task that requires immediate attention today!")
             else:
-                print(f"'{task}' is a high priority task.")
+                print(f"Reminder:\n'{task}' is a high priority task.")
         case 'medium':
             if time_bound == 'yes':
-                print(f"'{task}' is a medium priority task that requires immediate attention today!")
+                print(f"Reminder:\n'{task}' is a medium priority task that requires immediate attention today!")
             else:
-                print(f"'{task}' is a medium priority task.")
+                print(f"Reminder:\n'{task}' is a medium priority task.")
         case 'low':
             if time_bound == 'yes':
-                print(f"'{task}' is a low priority task that requires immediate attention today!")
+                print(f"Reminder:\n'{task}' is a low priority task that requires immediate attention today!")
             else:
-                print(f"'{task}' is a low priority task. Consider completing it when you have free time.")
+                print(f"Reminder:\n'{task}' is a low priority task. Consider completing it when you have free time.")
         case _:
             print("Invalid priority level entered.")
 
